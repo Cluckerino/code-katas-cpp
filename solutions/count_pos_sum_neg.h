8a7cf9a17cd41aa582402f55b45955aecbf3ac9a
Cluckerino <elitedragonslayer@gmail.com>
2018-12-05 14:31:56 -0800

    Added the solution

--- solutions/count_pos_sum_neg.h
+++ solutions/count_pos_sum_neg.h
@@ -3,5 +3,12 @@
 // Given a list of numbers, count the number of positive numbers and sum the negative numbers.
 std::vector<int> countPositivesSumNegatives(std::vector<int> input)
 {
-    return std::vector<int> {0, 0};
+    if (input.empty()) return input;
+    auto count = 0, sum = 0;
+    for (auto & num : input)
+    {
+        if (num > 0) ++count;
+        else if (num < 0) sum += num;
+    }
+    return std::vector<int> {count, sum};
 }
