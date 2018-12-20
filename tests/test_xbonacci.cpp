b2b7be9eb15768820323e856400ceaee8dee57ba
Cluckerino <elitedragonslayer@gmail.com>
2018-12-20 13:17:49 -0800

    Added the rest of the tests from the kata

--- tests/test_xbonacci.cpp
+++ tests/test_xbonacci.cpp
@@ -16,3 +16,43 @@ TEST(Xbonacci, KataEx2)
     auto actual = xbonacci(signature, 10);
     EXPECT_EQ(actual, expected);
 }
+
+TEST(Xbonacci, KataEx3)
+{
+    std::vector<int> signature = {0, 0, 0, 0, 1};
+    std::vector<int> expected = {0, 0, 0, 0, 1, 1, 2, 4, 8, 16};
+    auto actual = xbonacci(signature, 10);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(Xbonacci, KataEx4)
+{
+    std::vector<int> signature = {1, 0, 0, 0, 0, 0, 1};
+    std::vector<int> expected = {1, 0, 0, 0, 0, 0, 1, 2, 3, 6};
+    auto actual = xbonacci(signature, 10);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(Xbonacci, KataEx5)
+{
+    std::vector<int> signature = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::vector<int> expected = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 32, 64, 128, 256};
+    auto actual = xbonacci(signature, 20);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(Xbonacci, KataEx6)
+{
+    std::vector<int> signature = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    std::vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto actual = xbonacci(signature, 9);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(Xbonacci, KataEx7)
+{
+    std::vector<int> signature = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    std::vector<int> expected = {};
+    auto actual = xbonacci(signature, 0);
+    EXPECT_EQ(actual, expected);
+}
