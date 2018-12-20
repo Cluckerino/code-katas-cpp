a5b6b17c89897dd31d2c7c635f819c48354e8c19
Cluckerino <elitedragonslayer@gmail.com>
2018-12-20 13:39:38 -0800

    Solution for the sig < n edge case

--- solutions/xbonacci.h
+++ solutions/xbonacci.h
@@ -1,8 +1,18 @@
+#include <algorithm>
 #include <vector>
 
 // Given a signature of the first x-terms of an x-bonacci sequence, continue the sequence up to the
 // n-th term.
 std::vector<int> xbonacci(std::vector<int> signature, int n)
 {
+    std::vector<int> result;
+    auto bonacciLength = signature.size();
+
+    // If n is less than the signature, don't bother with iterating, just take the first n terms.
+    if (n < bonacciLength)
+    {
+        std::copy_n(begin(signature), n, std::back_inserter(result));
+        return result;
+    }
     return std::vector<int>{};
 }
