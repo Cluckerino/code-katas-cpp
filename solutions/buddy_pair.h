9c4688c7513b855516b3a6fe385fa6eecffe7019
Cluckerino <elitedragonslayer@gmail.com>
2018-12-21 13:11:47 -0800

    Added pair-to-string

---
+++ solutions/buddy_pair.h
@@ -0,0 +1,16 @@
+#include <string>
+
+std::string print_buddy_pair(long long num1, long long num2);
+
+// Find the first number between start and limit that has a buddy. A number a has a buddy b if
+// sum(proper divisors of a) - 1 = b and sum(proper divisors of b) - 1 = a.
+std::string buddy(long long start, long long limit)
+{
+    return print_buddy_pair(0, 0);
+}
+
+// Print the pair of numbers.
+std::string print_buddy_pair(long long num1, long long num2)
+{
+    return "(" + std::to_string(num1) + " " + std::to_string(num2) + ")";
+}
---
+++ tests/test_buddy_pair.cpp
@@ -0,0 +1,36 @@
+#include "../solutions/buddy_pair.h"
+#include <gtest/gtest.h>
+
+TEST(BuddyPair, PrintBuddyPair)
+{
+    std::string expected = "(1081184 1331967)";
+    auto actual = print_buddy_pair(1081184, 1331967);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(BuddyPair, KataEx1)
+{
+    long long start = 1071625;
+    long long limit = 1103735;
+    std::string expected = "(1081184 1331967)";
+    auto actual = buddy(start, limit);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(BuddyPair, KataEx2)
+{
+    long long start = 2382;
+    long long limit = 3679;
+    std::string expected = "Nothing";
+    auto actual = buddy(start, limit);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(BuddyPair, KataEx3)
+{
+    long long start = 8983;
+    long long limit = 13355;
+    std::string expected = "(9504 20735)";
+    auto actual = buddy(start, limit);
+    EXPECT_EQ(actual, expected);
+}
