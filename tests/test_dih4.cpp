798f65ddda0537657a29c882bb8c4a5b58668f13
Cluckerino <elitedragonslayer@gmail.com>
2018-12-23 22:11:21 -0800

    Added test for inverser and determinant

--- tests/test_dih4.cpp
+++ tests/test_dih4.cpp
@@ -20,3 +20,19 @@ TEST(Dih4, NotEqual)
     Dih4 b{-1, 0, 0, -1};
     EXPECT_TRUE(a != b);
 }
+
+TEST(Dih4, Inv)
+{
+    Dih4 input{1, 3, 2, 7};
+    Dih4 expected{7, -3, -2, 1};
+    auto actual = input.inv();
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(Dih4, Det)
+{
+    Dih4 input{2, -3, 1, 4};
+    auto expected = 11;
+    auto actual = input.det();
+    EXPECT_EQ(actual, expected);
+}
